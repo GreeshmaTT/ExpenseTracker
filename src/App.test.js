7ee5d5cdebb01f9149ba19f747fc2028cd8bbbda
GreeshmaThelli <greeshma.thankachan@thoughtworks.com>
2022-10-28 15:54:57 +0530

    Greeshma | initial commit

---
+++ src/App.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders Expense Tracker header', () => {
+  render(<App />);
+  const linkElement = screen.getByText('Expense Tracker');
+  expect(linkElement);
+});
