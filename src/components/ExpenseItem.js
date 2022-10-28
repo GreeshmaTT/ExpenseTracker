62004511140386a3975cab4b1527fc05e4f7110f
GreeshmaThelli <greeshma.thankachan@thoughtworks.com>
2022-10-28 19:46:27 +0530

    Greeshma | Add, Card component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,19 +1,20 @@
 import "./ExpenseItem.css";
 import "./ExpenseDate";
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 
 function ExpenseItem(props) {
   const expenseAmount = props.amount;
   const expenseTitle = props.title;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate expenseDate={props.date} />
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{"$" + expenseAmount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
