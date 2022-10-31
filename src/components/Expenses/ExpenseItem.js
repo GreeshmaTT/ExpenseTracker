bbd71c6e984fd530591f6cde1e9e1a11074ece96
GreeshmaThelli <greeshma.thankachan@thoughtworks.com>
2022-10-31 15:20:35 +0530

    Greeshma | add, components to add a new expense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,11 +2,16 @@ import "./ExpenseItem.css";
 import "../Expenses/ExpenseDate";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
+import { useState } from "react";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   const expenseAmount = props.amount;
-  const expenseTitle = props.title;
+  const [expenseTitle, setExpenseTitle] = useState(props.title);
 
+  const clickHandler = () => {
+    setExpenseTitle("Updated");
+    // console.log(expenseTitle);
+  };
   return (
     <Card className="expense-item">
       <ExpenseDate expenseDate={props.date} />
@@ -14,8 +19,9 @@ function ExpenseItem(props) {
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{"$" + expenseAmount}</div>
       </div>
+      <button onClick={clickHandler}>Change</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
